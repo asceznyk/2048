7713e06fafbb65e023906b0b008a75877d485f81
asceznyk <asceznyk@gmail.com>
2022-07-02 18:09:22 +0530

    ..

--- game.js
+++ game.js
@@ -1,8 +1,29 @@
 const canvas = document.getElementById('mycanvas');
 const ctx = canvas.getContext('2d');
-const scl = 20;
+const scl = 100;
 
 ctx.scale(scl, scl);
+
+let grid = [
+	[0,0,0,0],
+	[0,0,0,0],
+	[0,0,0,0].
+	[0,0,0,0],
+];
+
+function drawGrid(grid) {
+	ctx.fillStyle("#fff")
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			ctx.fillText(grid[i][j], i, j)
+		}
+	}
+}
+
+drawGrid(grid):
+
+
+
 
 
 
